5d4443ab4d4fad29cede5f5362c42242afbeb2a4
Tudor Daniel Andrei <tudordaniel56@gmail.com>
2023-08-27 22:02:00 +0300

    init project

---
+++ index.js
@@ -0,0 +1,6 @@
+const input = document.querySelector("#calculator-input-window");
+const calculateButton = document.querySelector(".calulate-button");
+var keyPressed;
+
+input.addEventListener("focus", () =>{
+})
