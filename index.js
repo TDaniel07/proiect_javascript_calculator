13359f3d073b5d5bedcbf994d6ee847b60e82aa1
Tudor Daniel Andrei <tudordaniel56@gmail.com>
2023-08-29 18:40:58 +0300

    first working version

--- index.js
+++ index.js
@@ -5,14 +5,156 @@ const subtractionButton = document.querySelector("#subtraction-button");
 const multiplicationButton = document.querySelector("#multiplication-button");
 const divisionButton = document.querySelector("#division-button");
 
-var keyPressed, number1, number2, result;
+var keyPressed, number1, number2, result, operation, isResult;
+
+number1 = null;
+number2 = null;
+result = null;
+operation = null;
+isResult = false;
+
+function calculate()
+{
+    debugger;
+    if(operation == null)
+        return;
+    switch(operation)
+    {
+        case "+":
+        if(number1 != null && number2 != null)
+        {
+            number1 += number2;
+            input.value = number1;
+            number2 = null;
+        }
+        else if(number1 != null && number2 == null)
+        {
+            if(input.value == "")
+            {
+                number1 += number1;
+                input.value = number1;
+                input.focus();
+            }
+            else
+            {
+                number2 = Number(input.value);
+                number1 += number2;
+                input.value = number1;
+                number2 = null;
+            }
+        }
+        break;
+        
+        case "-":
+        if(number1 != null && number2 != null)
+        {
+            number1 -= number2;
+            input.value = number1;
+            number2 = null;
+        }
+        else if(number1 != null && number2 == null)
+        {
+            if(input.value == "")
+            {
+                number1 -= number1;
+                input.value = number1;
+                input.focus();
+            }
+            else
+            {
+                number2 = Number(input.value);
+                number1 -= number2;
+                input.value = number1;
+                number2 = null;
+            }
+        }
+        break;
+        case "*":
+        if(number1 != null && number2 != null)
+        {
+            number1 *= number2;
+            input.value = number1;
+            number2 = null;
+        }
+        else if(number1 != null && number2 == null)
+        {
+            if(input.value == "")
+            {
+                number1 *= number1;
+                input.value = number1;
+                input.focus();
+            }
+            else
+            {
+                number2 = Number(input.value);
+                number1 *= number2;
+                input.value = number1;
+                number2 = null;
+            }
+        }
+        break;
+        
+        case "/":
+        if(number1 != null && number2 != null)
+        {
+            number1 /= number2;
+            input.value = number1;
+            number2 = null;
+        }
+        else if(number1 != null && number2 == null)
+        {
+            if(input.value == "")
+            {
+                number1 /= number1;
+                input.value = number1;
+                input.focus();
+            }
+            else
+            {
+                number2 = Number(input.value);
+                number1 /= number2;
+                input.value = number1;
+                number2 = null;
+            }
+        }
+        break;
+    }
+    isResult = true;
+}
+
+function calculatorOperation(operationSign)
+{
+    debugger;
+    operation = operationSign;
+
+    if(isResult)
+    {
+        input.value = "";
+        isResult = false;
+        input.focus();
+    }
+    else if(number1 == null && number2 == null)
+    {
+        number1 = Number(input.value);
+        input.value = "";
+        input.focus();
+    }
+    else if(number1 != null && number2 == null)
+    {
+        number2 = Number(input.value);
+        input.value = "";
+        input.focus();
+    }
+    else if(number1 != null && number2 != null)
+        calculate();
+}
 
 function hoverButtonColorChange(Button)
 {
-    Button.addEventListener("mouseover", () =>{
+    Button.addEventListener("mouseover", (e) =>{
         Button.classList.add("button-hover-color");
     })
-    Button.addEventListener("mouseleave", () =>{
+    Button.addEventListener("mouseleave", (e) =>{
         Button.classList.remove("button-hover-color");
     })
 }
@@ -30,16 +172,18 @@ input.addEventListener("keydown", (e) =>{
         return;
     }
     if(input.value[0] == '0')
-        input.value = input.value.slice(1);
+    input.value = input.value.slice(1);
 })
-
-number1 = null;
-number2 = null;
-result = null;
 
 hoverButtonColorChange(calculateButton);
 hoverButtonColorChange(additionButton);
 hoverButtonColorChange(subtractionButton);
 hoverButtonColorChange(multiplicationButton);
 hoverButtonColorChange(divisionButton);
+
+calculateButton.addEventListener("click", calculate);
+additionButton.addEventListener("click", () => {calculatorOperation("+")});
+subtractionButton.addEventListener("click", () => {calculatorOperation("-")});
+multiplicationButton.addEventListener("click", () => {calculatorOperation("*")});
+divisionButton.addEventListener("click", () => {calculatorOperation("/")});
 
